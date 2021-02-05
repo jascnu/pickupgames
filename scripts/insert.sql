62b9ba156b1a95b2f1ad9119f2c47a67cf789b82
Jared <jascnu@gmail.com>
2021-02-04 19:22:19 -0700

    Add inserts for dummy data

--- scripts/insert.sql
+++ scripts/insert.sql
@@ -2,9 +2,31 @@ INSERT INTO public.role(
 	roleid, rolename, roledescription)
 	VALUES (0, 'user', 'regular user');
 
-
 INSERT INTO public.pickupuser(
-	firstname, middle, lastname, email, username, attendancescore, passwordhash, zipcode, description, roleid)
-	VALUES ('Bronson', 'Scott', 'Puzey', 'bronsonisat@gmail.com', 'bpuzey', 0, 'p@ssw0rd', 84604, 'Placeholder', 0);
+	userid, firstname, middle, lastname, email, username, attendancescore, passwordhash, zipcode, description, roleid)
+	VALUES (0, 'Dummy', 'U', 'User', 'dummy@gmail.com', 'dummyuser', 0, 'p@ssw0rd', 1234, 'I am a dummy user', 0);
 
+INSERT INTO public.competitivelevels(
+	level, levelid)
+	VALUES ('Beginner', 0);
+
+INSERT INTO public.competitivelevels(
+	level, levelid)
+	VALUES ('Intermediate', 1);
+
+INSERT INTO public.competitivelevels(
+	level, levelid)
+	VALUES ('Advanced', 2);
+
+INSERT INTO public.location(
+	locationid, address, lat, "long", website, isopenaccess)
+	VALUES (0, '888 E 222 E', 12.2222, 12.2222, 'https://www.bing.com', true);
+
+INSERT INTO public.sports(
+	name, sportid)
+	VALUES ('Tennis', 0);
+
+INSERT INTO public.game(
+	gameid, title, playersrequired, iscompetitive, playersjoined, description, datetime, userid, sportid, locationid, levelid)
+	VALUES (0, 'Dummy Title', 20, true, 0, 'Dummy Description', to_timestamp(1612490936758), 0, 0, 0, 0);
 	
