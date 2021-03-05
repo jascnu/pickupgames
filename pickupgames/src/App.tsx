1c209dcb1223271a4dd82b6aed3a598f8eba80c9
Jared <jascnu@gmail.com>
2021-03-04 19:04:43 -0700

    Add signup and 404 page

--- pickupgames/src/App.tsx
+++ pickupgames/src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-
+import Signup from './pages/Signup';
+import PageNotFound from './pages/PageNotFound';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+	// </div>
+	<Router>
+		<div></div>
+		<Switch>
+			<Route exact path='/signup' component={Signup}></Route>
+			<Route component={PageNotFound}/>
+		</Switch>
+	</Router>
   );
 }
 
