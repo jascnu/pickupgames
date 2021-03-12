ad6274daf97faa3eb98292f1ad33ba6b4e7692c4
Jared <jascnu@gmail.com>
2021-03-11 19:32:33 -0700

    Add grant on sequences

--- scripts/backupuser.sql
+++ scripts/backupuser.sql
@@ -3,7 +3,9 @@ GRANT CONNECT ON DATABASE pickupgames TO backup_user;
 --Grant read to all tables
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO backup_user;
 --Automatically grant read access to new tables
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO backup_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT ON TABLES TO backup_user;
+
 --Make read only
 alter user backup_user set default_transaction_read_only = on;
