a9635d9ca97ec62dc6e0b580bbc1f849d5762f4e
Jared <jascnu@gmail.com>
2021-03-11 19:30:28 -0700

    Add backup scripts

---
+++ scripts/backupuser.sql
@@ -0,0 +1,9 @@
+create user backup_user with password 'notourpassword';
+GRANT CONNECT ON DATABASE pickupgames TO backup_user;
+--Grant read to all tables
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO backup_user;
+--Automatically grant read access to new tables
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT ON TABLES TO backup_user;
+--Make read only
+alter user backup_user set default_transaction_read_only = on;
